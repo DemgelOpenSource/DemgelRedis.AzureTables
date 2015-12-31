738d5419d9a23878bba68d4600674354d4391027
tenowg <tenowg@live.com>
2015-12-31 06:01:48 -0500

    powershell

--- Prebuild.ps1
+++ Prebuild.ps1
@@ -22,5 +22,5 @@ else
 
  # run DNU restore on all project.json files in the src folder including 2>1 to redirect stderr to stdout for badly behaved tools
 #Get-ChildItem -Path $PSScriptRoot\src -Filter project.json -Recurse | ForEach-Object { & dnu restore $_.FullName 2>1 }
-& dnu restore $PSScriptRoot\src\DemgelRedisTables\project.json
-& dnu restore $PSScriptToor\src\DemgelRedisTables.Tests\project.json
+& dnu restore $PSScriptRoot\src\DemgelRedis.AzureTables\project.json
+& dnu restore $PSScriptToor\src\DemgelRedis.AzureTables.Tests\project.json
